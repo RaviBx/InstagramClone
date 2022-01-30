13677cfcc25e124e80bd0c5a858c4add59300040
Ravishan Balasooriya <ravimuay@gmail.com>
2022-01-30 12:45:33 +0530

    Added main head section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,31 @@
 @section('content')
 <div class="container">
    
+    <div class="row">
+        <div class="col-3 p-5">
+
+            <img src="https://unilink.us/content/border/preview/1.png" class="rounded-circle">
+            
+
+        </div>
+
+        <div class="col-9 pt-5">
+
+            <div><h1>InstagramClone</h1></div>
+
+            <div class="d-flex">
+
+                    <div class="col-2 pl-1"><strong>153</strong>&nbsp;posts</div>
+                    <div class="col-2 pl-1"><strong>23k</strong>&nbsp;followers</div>
+                    <div class="pr-5"><strong>212</strong>&nbsp;following</div>
+
+            </div>
 
 
-    
+        </div>
+
+    </div>
+
+
 </div>
 @endsection
